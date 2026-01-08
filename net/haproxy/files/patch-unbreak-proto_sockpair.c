9b071b9e750ba6a366bdadef7cb6ed68c41a9880
Sergey Fedorov <barracuda@macos-powerpc.org>
2026-01-08 10:28:36 +0800

    haproxy: update patch, revbump

--- net/haproxy/files/patch-unbreak-proto_sockpair.c
+++ net/haproxy/files/patch-unbreak-proto_sockpair.c
@@ -1,13 +1,6 @@
-Reverts https://github.com/haproxy/haproxy/commit/10c14a1ed049707854614f28cd57e5fd43ec3b31
-
 --- src/proto_sockpair.c	2025-11-26 22:55:57.000000000 +0800
 +++ src/proto_sockpair.c	2025-12-22 05:16:41.000000000 +0800
-@@ -239,12 +239,12 @@
-  */
- int send_fd_uxst(int fd, int send_fd)
- {
--	char iobuf[2] = {0};
-+	char iobuf[2];
+@@ -237,12 +237,15 @@ int send_fd_uxst(int fd, int send_fd)
  	struct iovec iov;
  	struct msghdr msghdr;
  
@@ -18,3 +11,10 @@ Reverts https://github.com/haproxy/haproxy/commit/10c14a1ed049707854614f28cd57e5
  	struct cmsghdr *cmsg = (void *)buf;
  
  	int *fdptr;
+ 
++	memset(cmsgbuf, 0, sizeof(cmsgbuf));
++	memset(buf, 0, sizeof(buf));
++
+ 	iov.iov_base = iobuf;
+ 	iov.iov_len = sizeof(iobuf);
+ 
